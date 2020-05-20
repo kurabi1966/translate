8bb9138f7a1570907925ffa5499b0b7da33696e5
Mohammed Ammar Kurabi <kurabi@gmail.com>
2020-05-21 00:38:06 +0400

    Back to blanck App

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render = () => {
+    return <div>Translate Application that Uses React Context</div>;
+  };
+}
+
+export default App;
